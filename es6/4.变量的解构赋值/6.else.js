062edafe178db1478b7b9db47da239457c4d6f53
赵龙 <long.zhao@mytian.com.cn>
2018-09-13 12:00:09 +0800

    备份

--- es6/4.变量的解构赋值/6.else.js
+++ es6/4.变量的解构赋值/6.else.js
@@ -1,2 +1,29 @@
 // 其他基本类型的解构赋值
+// 1.尝试转为对象，例如bool，number
+// 2. null 或者 undefined 不能转为对象
+
+
+function test1() {
+    var x = 1
+    var {y} = x
+    console.log(y);
+    // undefined
+}
+// test1()
+function test2() {
+    // let {toString: a} = 123;
+    // console.log(a === Number.portotype.toString);
+
+    let {toFixed: s} = 123;
+     // true
+    if (s === Number.prototype.toFixed) {
+        console.log("it work");
+        
+    }else{
+        console.log("error");
+    }
+    
+}
+// test2()
+
 

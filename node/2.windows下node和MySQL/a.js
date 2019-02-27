d185b3a9fbceae7ed70a45da8a0c167991f75e4f
erlei22 <574414066@qq.com>
2019-02-27 14:11:20 +0800

    终于成功了

--- node/2.windows下node和MySQL/a.js
+++ node/2.windows下node和MySQL/a.js
@@ -5,7 +5,7 @@ var mysql  = require('mysql');  //调用MySQL模块
 var connection = mysql.createConnection({
     host     : '127.0.0.1',       //主机
     user     : 'root',               //MySQL认证用户名
-    password : '',        //MySQL认证用户密码
+    password : 'zhaolong',        //MySQL认证用户密码
     port: '3306',                   //端口号
 });
 //创建一个connection
@@ -17,12 +17,19 @@ connection.connect(function(err){
     console.log('[connection connect]  succeed!');
 });
 //执行SQL语句
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+connection.query('use testmysql;', function(err, rows, fields) {
     if (err) {
         console.log('[query] - :'+err);
         return;
     }
-    console.log('The solution is: ', rows[0].solution);
+    console.log('rows:', rows);
+});
+connection.query('SELECT *  FROM students;', function(err, rows, fields) {
+    if (err) {
+        console.log('[query] - :'+err);
+        return;
+    }
+    console.log('The solution is: ', rows);
 });
 //关闭connection
 connection.end(function(err){

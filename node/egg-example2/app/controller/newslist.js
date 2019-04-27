4291b2d7beb01ff81df6dc7daa28edfcb2cead45
赵龙 <15251928151@163.com>
2019-04-27 17:18:24 +0800

    temp

--- node/egg-example2/app/controller/newslist.js
+++ node/egg-example2/app/controller/newslist.js
@@ -1,13 +1,13 @@
 const Controller = require('egg').Controller;
 
-class newControler extends Controller {
-    async list(){
-        const dataList = {
-            list: [
-              { id: 1, title: 'this is news 1', url: '/news/1' },
-              { id: 2, title: 'this is news 2', url: '/news/2' }
-            ]
-    }
-};
- await this.ctx.render('news/list.tpl' , dataList) 
+class NewsController extends Controller {
+  async list() {
+    const dataList = {
+      list: [
+        { id: 1, title: 'this is news 1', url: '/news/1' },
+        { id: 2, title: 'this is news 2', url: '/news/2' }
+      ]
+    };
+    await this.ctx.render('news/list.tpl', dataList);
+  }
 }
